f43be348f17e81797df128ecb7c3a66cb4415c8e
Polyane Ribeiro <poly.pequena@gmail.com>
2022-05-19 19:56:58 -0300

    screen-home

---
+++ src/assets/icons/ArrowRight.tsx
@@ -0,0 +1,8 @@
+export const ArrowRight = ({width, height, stroke}: any) => {
+    return (
+        <svg width={width ? width : 13} height={height ? height : 21} viewBox="0 0 13 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1L11.0012 10.0021L1 19.0042" stroke={stroke ? stroke : "black"} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+
+    )
+}
---
+++ src/styles/home.ts
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100vh;
+`
+
+export const AplicationHome = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+    background-color: #835AFD;
+    opacity: 0.9;
+`
+
+export const Question = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 80%;
+    height: 60%;
+    background-color: #F8F8F8;
+    border-top-right-radius: 500px;
+    border-bottom-right-radius: 500px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px -50px 36px -28px inset;
+`
+
+export const RedirectPage = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 50%;
+    margin: 0 150px;
+`
+
+export const Ball = styled.div`
+    width: 50px;
+    height: 50px;
+    border-radius: 50px;
+    background-color: #F8F8F8;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px -50px 36px -28px inset;
+`
+
+export const Icon = styled.div`
+    width: 150px;
+    display: flex;
+    align-items: center;
+    padding: 50px;
+    border-radius: 500px;
+    background-color: #F8F8F8;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px -50px 36px -28px inset;
+    cursor: pointer;
+
+    :hover {
+        background-color: white;
+
+    }
+`
+
+export const Title = styled.h1`
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+  font-family: sans-serif;
+
+  a {
+    color: #835AFD;
+    text-decoration: none;
+    cursor: pointer;
+
+    :hover {
+        text-decoration: underline;
+    }
+  }
+`
